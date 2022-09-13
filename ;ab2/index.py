766feb158ab3446986499fdc2f699d9075369493
razkaz0 <xxxrenker@gmail.com>
2022-09-13 13:44:02 +0300

    feat(lab2): init

---
+++ ;ab2/index.py
@@ -0,0 +1,18 @@
+h=175 #сантиметры 
+w=69
+stepCount=10000
+time=60 #минуты
+steplengt=h/4 +0.37
+s=steplengt*stepCount/100 # из сантиметров в метры
+v=s/time
+energyCount=0.035* w +(v*v/h)*0.029*w
+a=s/1000
+print("Расстояние в м" , s)
+print("Кол-во энергии", energyCount)
+print("Дистанция в км" , a)
+if a<2:
+    print("Нужно ходить больше")
+elif a<4:
+    print("Ты почти у цели")
+else:
+    print("Ты супер крут")
